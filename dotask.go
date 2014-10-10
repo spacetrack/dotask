b49bca9d76cab90097857871104235f3646ad2c7
Bjoern Winkler <bwinkler@netmediaeurope.com>
2014-10-10 21:30:46 +0200

    initial checkin

---
+++ dotask.go
@@ -0,0 +1,176 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"github.com/spacetrack/dotask/task"
+	"io/ioutil"
+	"os"
+	"strings"
+	"time"
+	"sort"
+)
+
+func main() {
+	var tasks = map[string]*task.Task{}
+	var t *task.Task
+
+	var writeUpdate = false
+	var blob []byte
+	var err error
+
+	// only 1 argument? Fail!
+	if len(os.Args) < 2 {
+		fmt.Println("ERROR: invalid number of arugments; use \"dotask help\" for list of valid arguments")
+		os.Exit(-1)
+	}
+
+	blob, err = ioutil.ReadFile("my_tasks.json")
+	err = json.Unmarshal(blob, &tasks)
+
+	switch os.Args[1] {
+
+	// ---
+	// --- command "help"
+	// --- ... print the help text
+	// ---
+	case "--help", "help":
+		writeUpdate = false
+
+		fmt.Println("dotask (l)ist")
+		fmt.Println("dotask (n)ow <title>")
+		fmt.Println("dotask (c)ontinue ID [_now_|<timestamp>]")
+		fmt.Println("dotask delete ID")
+		fmt.Println("dotask shutdown")
+		fmt.Println()
+		fmt.Println("dotask ID")
+		fmt.Println("dotask ID asis|now|<timestamp> [<title> ...]")
+
+		os.Exit(0)
+
+	// ---
+	// --- command "list"
+	// --- ... print the help text
+	// ---
+	case "l", "list":
+		writeUpdate = false
+		thisDay := ""
+
+		timeline := []*task.Task{}
+
+		fmt.Println("Your tasks:")
+
+		for _, aTask := range tasks {
+			timeline = append(timeline, aTask)
+		}
+
+		sort.Sort(task.ByDate(timeline))
+
+		for _, aTask := range timeline {
+
+			if thisDay != aTask.Timestamp.Format("2006-01-02") {
+				thisDay = aTask.Timestamp.Format("2006-01-02")
+				fmt.Println("----------------------------------------")
+			}
+
+			fmt.Println(aTask)
+		}
+
+		os.Exit(0)
+
+	case "delete":
+		writeUpdate = true
+
+		t, ok := tasks[os.Args[2]]
+
+		if ok {
+			delete(tasks, os.Args[2])
+		} else {
+			fmt.Println("DELETE failed")
+			fmt.Println(ok)
+		}
+
+		fmt.Println("DELETED:")
+		fmt.Println(t)
+
+	// ---
+	// --- command "new"
+	// --- ... create a new task and store it
+	// ---
+	case "now":
+		writeUpdate = true
+
+		t = task.NewTask()
+		t.Timestamp = time.Now()
+		t.Title = strings.Join(os.Args[2:], " ")
+
+		tasks[t.Id] = t
+		fmt.Println(t)
+
+	default:
+		writeUpdate = true
+
+		// first: the task ID
+		t, ok := tasks[os.Args[1]]
+
+		if ok {
+			fmt.Println(t)
+		} else {
+			if os.Args[1] == "0" {
+				t = task.NewTask()
+			} else {
+				fmt.Println("unkown task id \"" + os.Args[1] + "\"; use \"0\" to create a new task")
+				os.Exit(-1)
+			}
+		}
+
+		// second: asis, now, time or date + time
+		switch os.Args[2] {
+		case "asis":
+			// nothing
+
+		case "now":
+			t.Timestamp = time.Now()
+
+		default:
+			// time only: "HH:mm"
+			if len(os.Args[2]) == 5 {
+				today := time.Now().Format("2006-01-02")
+				t.Timestamp, err = time.Parse("2006-01-02 15:04", today+" "+os.Args[2])
+			}
+
+			// date + time: "dd.mm.YYYY-HH:mm"
+			if len(os.Args[2]) == 16 {
+				t.Timestamp, err = time.Parse("02.01.2006-15:04", os.Args[2])
+			}
+
+			// date + time: "dd.mm.YY-HH:mm"
+			if len(os.Args[2]) == 14 {
+				t.Timestamp, err = time.Parse("02.01.06-15:04", os.Args[2])
+			}
+
+		}
+
+		// third: the title
+		t.Title = strings.Join(os.Args[3:], " ")
+
+		tasks[t.Id] = t
+		fmt.Println(t)
+
+	}
+
+	// ---
+	// --- store data ...
+	// ---
+
+	if writeUpdate {
+		blob, err = json.MarshalIndent(tasks, "", "	")
+
+		if err != nil {
+			fmt.Println("ERROR: can't print tasks as JSON [", err, "]")
+			os.Exit(-1)
+		}
+
+		ioutil.WriteFile("my_tasks.json", blob, 0777)
+	}
+}
---
+++ task/task.go
@@ -0,0 +1,48 @@
+package task
+
+import (
+	"math/rand"
+	"strconv"
+	"time"
+)
+
+type Task struct {
+	Id          string
+	Title       string
+	Timestamp   time.Time
+	External_ID string
+	Parent_ID   string
+}
+
+func NewTask() *Task {
+	rand.Seed(time.Now().UnixNano())
+	r := 1000 + rand.Intn(9999-1000)
+
+	return &Task{
+		Id: strconv.FormatInt(time.Now().Unix(), 10) + "-" + strconv.Itoa(r),
+	}
+}
+
+func (t *Task) String() string {
+	var result = ""
+
+	result = result + t.Timestamp.Format("2006-01-02 15:04") + " | "
+	result = result + t.Id + " | "
+	result = result + t.Title
+
+	return result
+}
+
+type ByDate []*Task
+
+func (t ByDate) Len() int {
+	return len(t)
+}
+
+func (t ByDate) Swap(i, j int) {
+	t[i], t[j] = t[j], t[i]
+}
+
+func (t ByDate) Less(i, j int) bool {
+	return t[i].Timestamp.Before(t[j].Timestamp)
+}
